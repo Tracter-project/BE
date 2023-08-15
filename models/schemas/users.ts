6cc44b273c88d7e6fec2b08f78c3f3c3814e115a
[SW5] 임채현 <limvsme@gmail.com>
2023-08-15 17:56:48 +0900

    Feat: User 스키마 생성

---
+++ models/schemas/users.ts
@@ -0,0 +1,47 @@
+import { Schema } from "mongoose";
+
+export interface Iuser extends Document {
+    email: string;
+    nickname: string;
+    password: string;
+    likes: Schema.Types.ObjectId;
+    role: string;
+    isDeleted: boolean;
+}
+
+// UserSchema
+const UserSchema: Schema<Iuser> = new Schema<Iuser> (
+    {
+        email : {
+            type: String,
+            required: true,
+        },
+        nickname: {
+            type: String,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        likes: {
+            type: Schema.Types.ObjectId,
+            ref: "Place",
+            required: false,
+        },
+        role: {
+            type: String,
+            enum: ['admin', 'member'],
+            default: 'member',
+            required: true,
+        },
+        isDeleted: {
+            type: Boolean,
+            required: false,
+        },
+        
+    },
+    {
+        timestamps: true,
+    }
+)
